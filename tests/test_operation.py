1674633af97fb828905987fbe4d17cc0c4a58785
unknown <islamoahidul12@gmail.com>
2025-03-14 21:58:54 +0600

    updated

---
+++ tests/test_operation.py
@@ -0,0 +1,10 @@
+from src.math_opearation import add, sub
+
+
+def test_add():
+    assert add (2,3) ==5
+    assert add (-1,1) == 0
+
+def test_sub():
+    assert sub (5,3) ==5
+    assert sub (4,1) == 0
